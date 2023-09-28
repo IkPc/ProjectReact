6a3db67b2c16b9ed3aa3fd645f8cab55d4aeb1de
Pabllo Cristian <114034403+IkPc@users.noreply.github.com>
2023-09-28 14:50:58 +0000

    Segundo commit

--- src/components/basic/Login.jsx
+++ src/components/basic/Login.jsx
@@ -16,7 +16,7 @@ export default function Login(){
                 <label for="age">Idade:</label><br />
                 <input id="age" type="number" min="0" max="100"></input><br /><br />
                 
-                <label for="termos">Ao clicar no botão você aceita nossos <a id="term" href="https://github.com/ikpc/projetoreact" target="_self">Termos de Serviço   </a></label>
+                <label for="termos">Ao clicar no botão você aceita nossos <a id="term" href="https://github.com/IkPc/ProjectReact" target="_self">Termos de Serviço   </a></label>
                 <input id="termos" type="checkbox"></input><br /><br />
 
                 <input id="submit" type="button" value="Fazer Login"></input>
